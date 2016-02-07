0f18333180ff0e03eac4562ed48c0f683400973d
Michael Wheeler <mwheeler@g2crowd.com>
2016-02-07 16:57:01 -0600

    Adding babel-loader, rewriting using ES6 syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,39 @@
 module.exports = {
   context: __dirname,
-  entry: './lib/entry.js',
+  entry: [
+    './lib/entry.js'
+  ],
+
+  externals: {
+    'jquery': 'jQuery'
+  },
 
   output: {
     path: './js/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    libraryTarget: 'umd'
   },
 
   resolve: {
     extensions: ['', '.js']
+  },
+
+  module: {
+    loaders: [
+      {
+        loader: "babel-loader",
+
+        // Skip any files outside of your project's `src` directory
+        exclude: /node_modules/,
+
+        // Only run `.js` and `.jsx` files through Babel
+        test: /\.js$/,
+
+        // Options to configure babel with
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
   }
 };
